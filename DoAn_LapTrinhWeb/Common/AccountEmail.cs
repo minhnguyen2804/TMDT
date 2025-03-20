9a5d4e1c94c7835278849905c2a6c31d743619b3
ngocngoc <nguyenngocminhb2804@gmail.com>
2025-03-20 16:02:22 +0700

    abc

---
+++ DoAn_LapTrinhWeb/Common/AccountEmail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_LapTrinhWeb.Common
+{
+    public class AccountEmail
+    {
+        //vui lòng không đổi mật khẩu mail, néu mật khảu bị thay đổi vui lòng nhắn tin qua zalo:0948521622
+        //để dùng google email gửi email reset cho người khác bạn cần phải vô đây "https://www.google.com/settings/security/lesssecureapps" Cho phép ứng dụng kém an toàn: Bật
+        public const string Host = "smtp.gmail.com"; //tên mấy chủ nếu bạn dùng gmail thì đổi  "Host = "smtp.gmail.com"
+        //tài khoản email của bạn
+        public const string UserEmail = "nguyenngocminhb2804@gmail.com";
+            //"suabottothandh@gmail.com";
+        public const string UserEmailSupport = "nguyenngocminhb2804@gmail.com";
+        //Mật khẩu Email của bạn
+        public const string Password = "nwsu ijjw swdo hdhw"; //nwsu ijjw swdo hdhw
+            //"hoan265104";
+        //Tên Email bạn muốn hiển thị khi gửi
+        public const string Name = "SuabottotH&H";
+    }
+}
